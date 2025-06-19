fc53c498797e22db1dec8f84e5231122814494f3
312spartan <ian.royer31@gmail.com>
2025-06-19 10:54:53 -0700

    Uploading todays practice

---
+++ dom-manipulation/dom-manipulation.js
@@ -0,0 +1,6 @@
+function changeText() {
+    document.getElementById("main-heading").textContent =
+    'I really like the Shrek movies, but Spiderman was better.';
+}
+
+console.log(document.getElementById("main-heading").textContent);
---
+++ dom-manipulation/project/readme.md
@@ -0,0 +1,9 @@
+# Interactive To-Do List
+
+## Goals
+
+## Let users add tasks dynamically
+
+## Let users remove tasks by clicking on them
+
+## use DOM manipulation to update the User Interface
