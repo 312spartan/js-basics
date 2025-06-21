747314a3b5912e967f10e4d57c9ae934eab1660b
312spartan <ian.royer31@gmail.com>
2025-06-20 17:08:11 -0700

    Pushing more practice from today

---
+++ events/project/readme.md
@@ -0,0 +1,7 @@
+# interactive color changer
+
+## Goals
+
+### User clicks buttons to change the background color of the page
+
+### Uses Event listeners for interactive behavior
---
+++ events/project/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".color-btn");
+const resetButton = document.getElementById("reset");
+
+
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        document.body.style.backgroundColor = button.getAttribute("data-color");
+    });
+});
+
+
+// reset the background color
+
+resetButton.addEventListener("click", function () {
+    document.body.style.backgroundColor = "white";
+});
+
+// const text = document.querySelectorAll(".color-btn");
+        
